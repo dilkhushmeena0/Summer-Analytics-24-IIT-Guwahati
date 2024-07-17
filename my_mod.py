662f60ce167188eab433126253b324a2ea72d2fd
Dilkhush Meena <121503465+dilkhushmeena0@users.noreply.github.com>
2024-07-17 19:02:29 +0530

    Delete my_mod.py

--- my_mod.py
+++
@@ -1,9 +0,0 @@
-print('my_mod..')
-test = 'Test String'
-
-def find_ind(to_search, target):
-    for i, val in enumerate(to_search):
-        if val == target:
-            return i
-        
-    return -1
